62d3987fd36859a31a6fce7026f73aa91363a15c
Swoxyi <154547570+Swoxyi@users.noreply.github.com>
2024-09-08 14:56:16 +0300

    LoginBar.java

--- src/main/java/me/sedattr/loginbar/LoginBar.java
+++ src/main/java/me/sedattr/loginbar/LoginBar.java
@@ -13,6 +13,7 @@ import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LoginBar extends JavaPlugin {
+    private static final String API_URL = "https://heisensetups.com.tr/app/api/v1/check.php?product=2&=xxx";
     public void loadVariables() {
         Variables.config = getConfig();
         Variables.bungeecord = Variables.config.getBoolean("bungeecord");
@@ -27,6 +28,7 @@ public class LoginBar extends JavaPlugin {
     }
 
     public void onEnable() {
+        licenseCheck();
         saveDefaultConfig();
         if (!Bukkit.getPluginManager().isPluginEnabled("AuthMe")) {
             Bukkit.getConsoleSender().sendMessage("§8[§bDeluxeBazaar§8] §cI can't find AuthMe! Plugin is disabling...");
@@ -50,6 +52,72 @@ public class LoginBar extends JavaPlugin {
         }
 
         Bukkit.getConsoleSender().sendMessage("§8[§bProLoginBar§8] §aPlugin is successfully enabled! §fv" + getDescription().getVersion());
+    }
+
+    private void licenseCheck() {
+        try {
+            String localIPAddress = getLocalIPAddress();
+            if (localIPAddress == null) {
+                System.out.println("Local IP address not found. Plugin is shutting down...");
+                return;
+            }
+
+            JSONObject response = sendGET(API_URL + localIPAddress);
+
+            Boolean status = (Boolean) response.get("status");
+            if (status != null && status) {
+                Bukkit.getConsoleSender().sendMessage("License found!");
+                Bukkit.getConsoleSender().sendMessage("Plugin is starting...");
+            } else {
+                Bukkit.getConsoleSender().sendMessage("License not found. Plugin is shutting down...");
+                Bukkit.getConsoleSender().sendMessage("License IP: " + localIPAddress);
+                Bukkit.shutdown();
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getLocalIPAddress() throws SocketException {
+        Enumeration interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface iface = (NetworkInterface) interfaces.nextElement();
+            if (iface.isLoopback() || !iface.isUp()) {
+                continue;
+            }
+            Enumeration addresses = iface.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress addr = (InetAddress) addresses.nextElement();
+                if (addr.isLinkLocalAddress() || addr.isLoopbackAddress() || addr.isMulticastAddress()) {
+                    continue;
+                }
+                return addr.getHostAddress();
+            }
+        }
+        return null;
+    }
+
+    private JSONObject sendGET(String url) throws IOException, ParseException {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = con.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            JSONParser parser = new JSONParser();
+            return (JSONObject) parser.parse(response.toString());
+        } else {
+            throw new IOException("Response code: " + responseCode);
+        }
     }
 
     public void onDisable() {
